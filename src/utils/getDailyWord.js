bfb4aa5120ed9d5a49854ae5e9b4faf3de4f0fe4
Josh Greiff <josh@greiff.org>
2025-05-31 20:37:21 -0400

    a

--- src/utils/getDailyWord.js
+++ src/utils/getDailyWord.js
@@ -1,3 +1,4 @@
+// src/utils/getDailyWord.js
 import { sha256 } from '@noble/hashes/sha256';
 import { utf8ToBytes } from '@noble/hashes/utils';
 import { fetchFiveLetterWords } from './fetchWordList';
@@ -14,6 +15,9 @@ export async function getDailyWord() {
   const wordList = await fetchFiveLetterWords();
   const seed = getDateSeed();
   const hash = sha256(utf8ToBytes(seed));
-  const index = parseInt(Buffer.from(hash).toString('hex').slice(0, 8), 16) % wordList.length;
+  const hex = Array.from(hash).map(b => b.toString(16).padStart(2, '0')).join('');
+  const index = parseInt(hex.slice(0, 8), 16) % wordList.length;
   return wordList[index];
 }
+
+export default getDailyWord;
